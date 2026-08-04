4d73425b1c054967c0a327692d1f4bfd3f8cb888
Basil <catalin.v.halatiu@gmail.com>
2026-08-04 15:01:56 +0200

    update UserMapper
    - remove try catch block, so exception thrown in UserDTO can bubble up in the ImporterService catch block
    - handle missing row keys (fallback to null)

--- src/Import/UserMapper.php
+++ src/Import/UserMapper.php
@@ -3,24 +3,18 @@
 namespace App\Import;
 
 use App\DTO\UserDTO;
-use Exception;
 
 final class UserMapper
 {
     public function mapDto(array $row): UserDTO
     {
-        try {
-            $userDTO = UserDTO::create();
+        $userDTO = UserDTO::create();
 
-            $userDTO->name = $row['Name'];
-            $userDTO->email = $userDTO->validateEmail($row['Email']);
-            $userDTO->role = $row['Role'];
-            $userDTO->isActive = $userDTO->normalizeBooleanValue($row['isActive']);
+        $userDTO->name = $row['Name'] ?? null;
+        $userDTO->email = $userDTO->validateEmail($row['Email'] ?? null);
+        $userDTO->role = $row['Role'] ?? null;
+        $userDTO->isActive = $userDTO->normalizeBooleanValue($row['isActive'] ?? null);
 
-            return $userDTO;
-
-        } catch (Exception $e) {
-            throw new \RuntimeException("An Exception occurred while trying to extract data from import: {$e->getMessage()}");
-        }
+        return $userDTO;
     }    
 }
