407890d5abb0f536695142835dd5505ff351ebb6
Basil <catalin.v.halatiu@gmail.com>
2026-08-05 12:13:08 +0200

    update BatchService
    - remove inheritance from AbstractController
    - configure batch channel and batch handler in monolog.yaml
    - inject LoggerInterface for memory usage

--- src/Service/BatchService.php
+++ src/Service/BatchService.php
@@ -3,21 +3,23 @@
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-// das extends abstractcontroller weg machen nachdem fertig mit debuggen
-class BatchService extends AbstractController
+class BatchService
 {
-    protected int $processed = 0;
+    public int $processed = 0;
     private int $batchSize = 500;
 
     public function __construct(
-        protected EntityManagerInterface $em
+        protected EntityManagerInterface $em,
+        #[Autowire(service: 'monolog.logger.batch')]
+        private LoggerInterface $batchLogger
     )
     {
     }
 
-    protected function handleBatchClearCycle(): void
+    public function handleBatchClearCycle(): void
     {
         if ($this->processed % $this->batchSize !== 0) {
             return;
@@ -30,35 +32,27 @@ class BatchService extends AbstractController
     {
         // debug lines to check memory usage
 
-        $memory = sprintf(
-        "verbraucht=%.2fMB available=%.2fMB peak=%.2fMB\n",
-        memory_get_usage(false) / 1024 / 1024,
-        memory_get_usage(true) / 1024 / 1024,
-        memory_get_peak_usage(true) / 1024 / 1024
-        );
-
-        // debug lines to check memory usage
-        $path = $this->getParameter('kernel.project_dir') . '/var/log/newlog.log';
-        $file = fopen($path, 'a');
-        fwrite($file, "BEFORE FLUSH AND CLEAR: {$memory}");
+        $this->batchLogger->debug('before flush / clear: ' . $this->getMemoryUsage());
 
         $this->em->flush();
         $this->em->clear();
         gc_collect_cycles();
 
-        // also debug lines to check memory
-        $memory2 = sprintf(
-        "verbraucht=%.2fMB available=%.2fMB peak=%.2fMB\n",
-        memory_get_usage(false) / 1024 / 1024,
-        memory_get_usage(true) / 1024 / 1024,
-        memory_get_peak_usage(true) / 1024 / 1024
-        );
-
-        fwrite($file, "AFTER CLEAR: {$memory2}\n");
-        fclose($file);
+        $this->batchLogger->debug('after flush / clear: ' . $this->getMemoryUsage());
     }
+
     public function finalize(): void
     {
         $this->em->flush();
+    }
+
+    private function getMemoryUsage(): string
+    {
+        return sprintf(
+            "verbraucht=%.2fMB available=%.2fMB peak=%.2fMB\n",
+            memory_get_usage(false) / 1024 / 1024,
+            memory_get_usage(true) / 1024 / 1024,
+            memory_get_peak_usage(true) / 1024 / 1024
+            );
     }
 }
