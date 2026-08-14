4f95a0f9ad8bd1664435cc3cb26c6a9c0c9b894f
Basil <catalin.v.halatiu@gmail.com>
2026-08-14 23:58:00 +0200

    add integration test for ImportFileUploadHandler
    - move ImportServiceTest to dedicated directory

---
+++ tests/Integration/Handler/ImportFileUploadHandlerTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Tests\Unit\Handler;
+
+use App\Entity\ImportFile;
+use App\Enum\FileTypeEnum;
+use App\Enum\ImportStatusEnum;
+use App\Handler\ImportFileUploadHandler;
+use App\Repository\ImportFileRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
+use LogicException;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Storage\StorageInterface;
+
+final class ImportFileUploadHandlerTest extends KernelTestCase
+{
+    private ImportFile $importFile;
+
+    public function setUp(): void
+    {
+        $this->importFile = new ImportFile();
+        
+        $this->importFile->fileName = 'users.csv';
+        $this->importFile->fileType = FileTypeEnum::CSV;
+        $this->importFile->status = ImportStatusEnum::STATUS_UPLOADED;
+        $this->importFile->uploadedAt = new \DateTimeImmutable();
+        $this->importFile->updatedAt = new \DateTimeImmutable();
+    }    
+
+    public function testHandlesFileUpload(): void
+    {
+        self::bootKernel();
+
+        $importFileUploader = self::getContainer()->get(ImportFileUploadHandler::class);
+        $repository = self::getContainer()->get(ImportFileRepository::class);
+        $storage = self::getContainer()->get(StorageInterface::class);
+
+        $tmp = sys_get_temp_dir() . '/' . uniqid() . '.xml';
+        copy(__DIR__ . '/../../Fixtures/Files/test.xml', $tmp);
+        $this->importFile->setFile(new UploadedFile($tmp, 'test.xml', null, null, true));
+        
+        $importFileUploader->handleUploadedFile($this->importFile);
+        $importFile = $repository->findOneBy([], ['id' => 'DESC']);
+
+        $this->assertSame(1, $repository->count([]));
+        $this->assertFileExists($storage->resolvePath($this->importFile, 'file'));
+        $this->assertSame(ImportStatusEnum::STATUS_UPLOADED, $importFile->status);
+        $this->assertSame(FileTypeEnum::XML, $importFile->fileType);
+    }
+
+    public function testDeleteThrowsIfPathDoesNotExist(): void
+    {
+        $this->expectException(LogicException::class);
+        $em = self::getContainer()->get(EntityManagerInterface::class);
+
+        $importFileUploader = self::getContainer()->get(ImportFileUploadHandler::class);
+        $em->persist($this->importFile);
+        $em->flush();
+
+        $importFileUploader->deleteUploadedFile($this->importFile);
+    }
+
+    public function testUploadThrowsIfPathDoesNotExist(): void
+    {
+        $this->expectException(FileNotFoundException::class);
+        $em = self::getContainer()->get(EntityManagerInterface::class);
+
+        $importFileUploader = self::getContainer()->get(ImportFileUploadHandler::class);
+        $em->persist($this->importFile);
+        $em->flush();
+
+        $importFileUploader->handleUploadedFile($this->importFile);
+    }
+
+    public function testDeletesUploadedFile(): void
+    {
+        $importFileUploader = self::getContainer()->get(ImportFileUploadHandler::class);
+        $repository = self::getContainer()->get(ImportFileRepository::class);
+
+        $tmp = sys_get_temp_dir() . '/' . uniqid() . '.xml';
+        copy(__DIR__ . '/../../Fixtures/Files/test.xml', $tmp);
+        $this->importFile->setFile(new UploadedFile($tmp, 'test.xml', null, null, true));
+        $importFileUploader->handleUploadedFile($this->importFile);
+
+        $this->assertFileExists($this->importFile->file->getPathname());
+
+        $importFileUploader->deleteUploadedFile($this->importFile);
+
+        $this->assertFileDoesNotExist($this->importFile->file->getPathname());
+        $this->assertSame(0, $repository->count([]));
+    }
+
+    public function tearDown(): void
+    {
+        self::bootKernel();
+        $fs = self::getContainer()->get(Filesystem::class);
+
+        if (null !== $this->importFile->file && $fs->exists($this->importFile->file->getPathname())) {
+            $fs->remove($this->importFile->file->getPathname());
+        }
+    }
+}
