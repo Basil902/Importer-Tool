00d42d447a100188ec5a28f1ea3c8c9fe6644718
Basil <catalin.v.halatiu@gmail.com>
2026-09-09 00:06:17 +0200

    fix mapping in ImportFile

--- src/Entity/ImportFile.php
+++ src/Entity/ImportFile.php
@@ -37,7 +37,7 @@ class ImportFile implements \Serializable
     #[ORM\Column(length: 255, enumType: ImportStatusEnum::class)]
     public ImportStatusEnum $status;
 
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'import_files')]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'importFile')]
     #[JoinColumn(nullable: false, onDelete: 'CASCADE')]
     private User $owner;
 
