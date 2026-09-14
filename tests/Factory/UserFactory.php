3233c3408e26fdf36330fbc3e41d48bbbf87dcf2
Basil <catalin.v.halatiu@gmail.com>
2026-09-14 16:53:25 +0200

    update UserFactory hash user password upon creation

--- tests/Factory/UserFactory.php
+++ tests/Factory/UserFactory.php
@@ -3,14 +3,19 @@
 namespace App\Tests\Factory;
 
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class UserFactory
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public function create(): User
     {
         $user = new User();
         $user->email = 'testuser@mail.com';
-        $user->setPassword('password');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
 
         return $user;
     }
